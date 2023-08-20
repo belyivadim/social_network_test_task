2f7a9954fae4e2ddfb7b45d99f03165329dd8fe3
belyivadim <belyivadim01@gmail.com>
2023-08-20 22:44:28 +0300

    try-except block for converting date from request

--- src/routes/analytics.py
+++ src/routes/analytics.py
@@ -1,14 +1,16 @@
 from datetime import datetime
-from flask import make_response
+from flask import abort, make_response
 from sqlalchemy import and_
 
-from config import app
 from models.like import Like
 
 def count_likes_for_period(date_from: str, date_to: str):
 
-    start = datetime.strptime(date_from, "%Y-%m-%d").date()
-    end = datetime.strptime(date_to, "%Y-%m-%d").date()
+    try:
+        start = datetime.strptime(date_from, "%Y-%m-%d").date()
+        end = datetime.strptime(date_to, "%Y-%m-%d").date()
+    except ValueError:
+        abort(400, "Date in wrong format.")
 
     likes = Like.query.filter(and_(
             Like.date_of_creation >= start,
